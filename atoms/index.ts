6075eae5dc7eedad465304817283febca8376220
azulrossini <mazulrossini@gmail.com>
2026-03-09 18:15:54 +0100

    Fixing data not showing

--- atoms/index.ts
+++ atoms/index.ts
@@ -1,10 +1,11 @@
 import { atomWithStorage } from 'jotai/utils';
 import { Exercise, Routine, Workout } from '@/types';
+import { mockExercises, mockRoutines, mockWorkouts } from '@/data/mockData';
 
-// Data atoms with localStorage persistence
-export const exercisesAtom = atomWithStorage<Exercise[]>('exercises', []);
-export const routinesAtom = atomWithStorage<Routine[]>('routines', []);
-export const workoutsAtom = atomWithStorage<Workout[]>('workouts', []);
+// Data atoms with localStorage persistence - initialized with mock data
+export const exercisesAtom = atomWithStorage<Exercise[]>('exercises', mockExercises);
+export const routinesAtom = atomWithStorage<Routine[]>('routines', mockRoutines);
+export const workoutsAtom = atomWithStorage<Workout[]>('workouts', mockWorkouts);
 
 // Theme atom - default to dark
 export const themeAtom = atomWithStorage<'light' | 'dark'>('theme', 'dark');
